8c082e22ba2a167b15435236383437ebf3b6ded9
Adam Newhouser <adamnewhouser@gmail.com>
2019-11-01 12:20:32 -0400

    complete

---
+++ src/store.js
@@ -0,0 +1,18 @@
+const bookmarks = [
+    {
+        id: 1,
+        title: 'Google',
+        url: 'http://www.google.com',
+        description: 'Google Search Engine',
+        Rating: 5
+    },
+    {
+        id: 2,
+        title: 'Amazon',
+        url: 'http://www.amazon.com',
+        desciption: 'Amazon shopping',
+        Rating: 5
+    }
+]
+
+module.exports = { bookmarks };
